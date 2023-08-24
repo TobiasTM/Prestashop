56d9b4a71dbb4aecd260b18518a13e79dadd30d2
Tobias Bogarin <todomicro.op18@gmail.com>
2023-08-24 13:36:04 -0300

    csv con el nro correspondiente de pedido

--- txthtml.py
+++ txthtml.py
@@ -28,15 +28,13 @@ def obtener_codigo_fuente_chrome(title):
     chrome_window.activate()
     time.sleep(1)
     pyautogui.hotkey('ctrl', 'u')
-    time.sleep(2)
+    time.sleep(3)
     pyautogui.hotkey('ctrl', 'a')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.hotkey('ctrl', 'c')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.hotkey('ctrl', 'w')
     return pyperclip.paste()  # Usa pyperclip.paste en lugar de pyautogui.paste
-
-html_content = obtener_codigo_fuente_chrome(title)
 
 html_content = obtener_codigo_fuente_chrome(title)
 
@@ -54,6 +52,7 @@ def extract_information(soup):
     product_elements = soup.find_all(lambda tag: tag.name == "a" and "Número de referencia:" in tag.get_text())
 
     detalle_data = []
+    total_quantity = 0  # Inicializar la suma total de la cantidad
     for product_element in product_elements:
         reference_parts = product_element.get_text().split('Número de referencia:')
         product_reference = reference_parts[1].strip() if len(reference_parts) > 1 else 'N/A'
@@ -65,6 +64,13 @@ def extract_information(soup):
         product_price = product_price_element.get_text().strip() if product_price_element else 'N/A'
 
         detalle_data.append([product_reference, product_quantity, product_price])
+                # Sumar la cantidad al total (asegurándose de que sea un número)
+        if product_quantity != 'N/A':
+            total_quantity += int(product_quantity)
+        
+        detalle_data.append([product_reference, product_quantity, product_price])
+        
+        
 
     # Extraer información del pedido
     order_info_element = soup.select_one('h1.page-title')
@@ -79,24 +85,32 @@ def extract_information(soup):
         order_number = 'N/A'
         recipient_name = 'N/A'
 
-    datos_data = [[order_number, product_quantity, order_url, recipient_name]]
+    datos_data = [[order_number, total_quantity, order_url, recipient_name]]  # Usar total_quantity aquí
 
     return detalle_data, datos_data
 
 
 detalle, datos = extract_information(soup)
 
+# Obtenemos el número de pedido de los datos recolectados
+order_number = datos[0][0]  # Asumiendo que datos[0][0] contiene el número de pedido
 
+# Nombre del archivo CSV para detalles
+detalle_filename = f"{order_number} DETALLES.csv"
 
-# Escribir detalles en un archivo CSV
-with open('detalle.csv', 'w', encoding='utf-8', newline='') as file:
+# Escribir detalles en el archivo CSV
+with open(detalle_filename, 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Número de Referencia', 'Cantidad', 'Precio del Producto'])
     writer.writerows(detalle)
 
-with open('datos.csv', 'w', encoding='utf-8', newline='') as file:
+# Nombre del archivo CSV para datos
+datos_filename = f"{order_number} DATOS.csv"
+
+# Escribir datos en el archivo CSV
+with open(datos_filename, 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Información del Pedido', 'Cantidad', 'URL del Pedido', 'Destinatario'])  # Agregada una nueva columna "Destinatario"
+    writer.writerow(['Información del Pedido', 'Cantidad', 'URL del Pedido', 'Destinatario'])
     writer.writerows(datos)
 
-print("La información ha sido guardada en detalle.csv y datos.csv.")
+print(f"La información ha sido guardada en {detalle_filename} y {datos_filename}.")
